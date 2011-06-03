ac73633d2058ab62df4eb1606da9aa416d6b4d8b
kthakore <thakore.kartik@gmail.com>
2011-06-03 18:57:40 -0400

    Cleaned up perltidy

--- lib/ZT/View.pm
+++ lib/ZT/View.pm
@@ -2,14 +2,31 @@ package ZT::View;
 use Modern::Perl;
 use Moose;
 use SDLx::App;
+use SDL::Video;
 
-has 'video' => ( is => 'rw', isa => 'SDLx::App', default => sub{ SDLx::App->new( title => "Zombie Trap", eoq => 1) } );
+has 'video' => (
+    is      => 'rw',
+    isa     => 'SDLx::App',
+    builder => '_build_video'
+);
+
+sub _build_video {
+    my $self = shift;
+
+    my $app = SDLx::App->new(
+        title => "Zombie Trap",
+        eoq   => 1,
+        flags => SDL_HWSURFACE | SDL_DOUBLEBUF
+    );
+
+    return $app;
+}
 
 sub run {
 
-	my $self = shift;
+    my $self = shift;
 
-	$self->video()->run();
+    $self->video()->run();
 
 }
 
