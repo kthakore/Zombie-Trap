c046854ef9f236e6373d855431dcaa5479c1c35d
kthakore <thakore.kartik@gmail.com>
2011-06-06 21:39:25 -0400

    Added type for game states

--- lib/ZT/Control.pm
+++ lib/ZT/Control.pm
@@ -1,14 +1,23 @@
 package ZT::Control;
 use Modern::Perl;
 use Moose;
+use Moose::Util::TypeConstraints;
 use ZT::Actor;
 use Box2D;
+
+our @STATES = qw/ menu game /;
+
+subtype 'GameState'
+	=> as 'Str'
+	=> where { $_ ~~ @STATES }
+	=> message { 'Invalid game state' }; 
 
 has 'actors' =>
   ( is => 'rw', isa => 'ArrayRef[ZT::Actor]', default => sub { [] } );
 has 'world' =>
   ( is => 'rw', isa => 'Box2D::b2World', builder => '_build_world' );
-
+has 'state' =>
+  ( is => 'rw', isa => 'GameState', default => sub { return 'menu'} );
 sub _build_world {
 
     my $vec = Box2D::b2Vec2->new( 0, 0 );
