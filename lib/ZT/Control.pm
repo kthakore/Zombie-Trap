c4af0c8b38a5888af82e2441555d5199fea9aac3
kthakore <thakore.kartik@gmail.com>
2011-06-06 23:03:46 -0400

    Addded deps and perl tided

--- lib/ZT/Control.pm
+++ lib/ZT/Control.pm
@@ -7,17 +7,16 @@ use Box2D;
 
 our @STATES = qw/ menu game /;
 
-subtype 'GameState'
-	=> as 'Str'
-	=> where { $_ ~~ @STATES }
-	=> message { 'Invalid game state' }; 
+subtype 'GameState' => as 'Str' => where { $_ ~~ @STATES } =>
+  message { 'Invalid game state' };
 
 has 'actors' =>
   ( is => 'rw', isa => 'ArrayRef[ZT::Actor]', default => sub { [] } );
 has 'world' =>
   ( is => 'rw', isa => 'Box2D::b2World', builder => '_build_world' );
 has 'state' =>
-  ( is => 'rw', isa => 'GameState', default => sub { return 'menu'} );
+  ( is => 'rw', isa => 'GameState', default => sub { return 'menu' } );
+
 sub _build_world {
 
     my $vec = Box2D::b2Vec2->new( 0, 0 );
